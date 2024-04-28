1fba3af74fe5ad432ba7be08e5fb85d17d276689
Trupti671 <truptikure01@gmail.com>
2024-04-28 18:56:31 +0530

    workforce

--- crmfrontend/src/Components/Workforce.js
+++ crmfrontend/src/Components/Workforce.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import '../css/workforce.css'
+import search from '../Images/search.png'
+import plus from '../Images/plus.png'
+import Designation from './Designation'
+import Profiles from './Profiles'
 
 function Workforce() {
   return (
-    <div>Workforce</div>
+    <>
+    <div className="wMain">
+      <div className="wTop">
+      <div className="searchbar">
+        <img src={search} alt="" />
+        <input type="text" placeholder='Search Profiles' />
+      </div>
+
+      <div className="createprof">
+        <img src={plus} alt="" />
+        <p>Create Profiles</p>
+      </div>
+      </div>
+
+      <div className="designation">
+      <Designation/>
+      <Profiles/>
+      </div>
+
+
+    </div>
+    </>
   )
 }
 
